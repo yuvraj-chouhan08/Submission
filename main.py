0d223446b350add0585added6b9ac38bd40773a1
yuvraj-chouhan08 <98700962+yuvraj-chouhan08@users.noreply.github.com>
2023-06-27 02:30:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,12 +30,16 @@ l1=[1, 3, 2, 2, -4, -6, -2, 8]
 tar=4
 sol=Solution(l1,tar)
 n1=sol.comb_sum(l1,tar)
-print(n1)
+print('First Combination For “4” :',n1)
 n2=sol.flatten(n1)
-print(n2)
+print("Merge Into a single Array :",n2)
 n3=sol.comb2(n2)
-print(n3)
+print("Second Combination For “8” :",n3)
 
+#Output
+# First Combination For “4” : [[1, 3], [2, 2], [-4, 8]]
+# Merge Into a single Array : [-4, 1, 2, 2, 3, 8]
+# Second Combination For “8” : [[8], [-4, 1, 3, 8], [-4, 2, 2, 8], [1, 2, 2, 3]]
         
             
         
